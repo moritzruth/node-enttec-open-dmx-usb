e16af171a7ef902cb0361aa7f511a2d71a237feb
Moritz Ruth <dev@moritzruth.de>
2023-05-16 15:42:14 +0200

    Allow providing a usleep-compatible function to account for setTimeout inaccuracies
    Also: Update dependencies

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # node-enttec-open-dmx-usb 🔌
 > A Node.js library for interacting with the
-> [Enttec Open DMX USB Interface](https://www.enttec.co.uk/en/product/controls/dmx-usb-interfaces/open-dmx-usb/)
+> [Enttec Open DMX USB interface](https://www.enttec.co.uk/en/product/controls/dmx-usb-interfaces/open-dmx-usb/)
 
 As it uses `serialport` under the hood, it should also work in
 [these environments](https://serialport.io/docs/guide-platform-support#supported-platforms-and-architectures).
@@ -8,7 +8,7 @@ As it uses `serialport` under the hood, it should also work in
 ## Install
 ![npm](https://img.shields.io/npm/v/enttec-open-dmx-usb?style=flat-square)
 
-Minimum required Node.js version is `v14.0.0`.
+The minimum required Node.js version is `v18.0.0`.
 
 ```sh
 yarn add enttec-open-dmx-usb
@@ -20,7 +20,7 @@ npm install enttec-open-dmx-usb
 [**View documentation on jsdocs.io**](https://www.jsdocs.io/package/enttec-open-dmx-usb#EnttecOpenDMXUSBDevice)
 
 ```js
-import { EnttecOpenDMXUSBDevice as DMXDevice } from "enttec-open-dmx-usb";
+import { EnttecOpenDMXUSBDevice as DMXDevice } from "enttec-open-dmx-usb"
 
 (async () => {
   const device = new DMXDevice(await DMXDevice.getFirstAvailableDevice())
@@ -38,7 +38,31 @@ import { EnttecOpenDMXUSBDevice as DMXDevice } from "enttec-open-dmx-usb";
 })()
 ```
 
+### What to do when this doesn’t work
+
+TLDR: Because `setTimeout` is imprecise, install [`easy-sleep`](https://github.com/qufei1993/easy-sleep) and
+pass the `usleep` function provided by it as the third parameter to the constructor of `EnttecOpenDMXUSBDevice`.
+See the example below.
+
+From [the Node.js documentation](https://nodejs.org/api/timers.html#settimeoutcallback-delay-args) regarding `setTimeout`:
+> Node.js makes no guarantees about the exact timing of when callbacks will fire, nor of their ordering.
+> The callback will be called as close as possible to the time specified.
+
+Because of this and the passive nature of the Enttec Open DMX USB interface, sometimes the timing requirements of the DMX specification are not met.
+Using a library such as [`easy-sleep`](https://github.com/qufei1993/easy-sleep) which allows sleeping (i. e. blocking the event loop) for a precise
+amount of microseconds is a possible workaround.
+
+You may pass a function sleeping for `n` *micro*seconds as the third parameter to the constructor of `EnttecOpenDMXUSBDevice`.
+
+For example, using `easy-sleep`:
+```js
+import { EnttecOpenDMXUSBDevice as DMXDevice } from "enttec-open-dmx-usb"
+import easySleep from "easy-sleep"
+
+new DMXDevice(await DMXDevice.getFirstAvailableDevice(), true, easySleep.Thread.usleep)
+```
+
 ## Events
-`ready` - `startSending` can be called.
+`ready` - `startSending` may be called.
 
 `error` - An error occurred. `error` events from `serialport` are passed through.
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { EventEmitter } from "eventemitter3"
-import SerialPort from "serialport"
+import { SerialPort } from "serialport"
 
 export const VENDOR_ID = "0403" // Enttec
 export const PRODUCT_ID = "6001" // Open DMX USB
@@ -9,21 +9,26 @@ interface Events {
   error: [Error]
 }
 
+type Usleep = (microSeconds: number) => unknown
+
 export class EnttecOpenDMXUSBDevice extends EventEmitter<Events> {
   private shouldBeSending = false
   private sendTimeout: ReturnType<typeof setTimeout> | null = null
   private buffer = Buffer.alloc(513)
   private readonly port: SerialPort
+  private readonly usleep: Usleep | null
 
   /**
-   * @param {string} path A path returned by {@link EnttecOpenDMXUSBDevice.listDevices} or
+   * @param path A path returned by {@link EnttecOpenDMXUSBDevice.listDevices} or
    * {@link EnttecOpenDMXUSBDevice.getFirstAvailableDevice}.
-   * @param {boolean} [startSending=true] Whether the device should start sending as soon as it is ready.
+   * @param [startSending=true] Whether the device should start sending as soon as it is ready.
+   * @param [usleep=null] A function blocking the event loop for `n` microseconds. See the README.md for more information.
    */
-  constructor(path: string, startSending = true) {
+  constructor(path: string, startSending = true, usleep: Usleep | null = null) {
     super()
 
-    this.port = new SerialPort(path, {
+    this.port = new SerialPort({
+      path,
       baudRate: 250000,
       dataBits: 8,
       stopBits: 2,
@@ -40,12 +45,14 @@ export class EnttecOpenDMXUSBDevice extends EventEmitter<Events> {
     this.port.on("error", (error: Error) => {
       this.emit("error", error)
     })
+
+    this.usleep = usleep
   }
 
   /**
    * Start sending.
-   * @param {number} [interval=0] The time between each attempt to send. Most of the time, `0` works.
-   * @throws Error When the device is not ready yet.
+   * @param [interval=0] The milliseconds between each attempt to send. Most of the time `0` works fine.
+   * @throws When the device is not ready yet.
    */
   startSending(interval = 0) {
     if (!this.port.isOpen) throw new Error("The device is not ready yet. Wait for the 'ready' event.")
@@ -54,7 +61,7 @@ export class EnttecOpenDMXUSBDevice extends EventEmitter<Events> {
 
     // eslint-disable-next-line unicorn/consistent-function-scoping
     const send = () => {
-      this.sendUniverse()
+      this._sendUniverse()
         .then(() => {
           if (this.shouldBeSending)
             // eslint-disable-next-line @typescript-eslint/no-misused-promises
@@ -76,11 +83,11 @@ export class EnttecOpenDMXUSBDevice extends EventEmitter<Events> {
   }
 
   /**
-   * Set the channel values.
+   * Set channel values.
    * If channels is an Object, the keys are the channel numbers.
    *
-   * @param {Buffer|Object|Array} channels
-   * @param {boolean} [clear=false] Whether all previously assigned channels should be set to 0
+   * @param channels
+   * @param [clear=false] Whether all previously assigned channels should be set to `0`
    */
   setChannels(channels: Buffer | number[] | Record<number, number>, clear = false) {
     if (clear) {
@@ -119,26 +126,34 @@ export class EnttecOpenDMXUSBDevice extends EventEmitter<Events> {
   }
 
   /**
-   * @returns {Promise} Resolves when the whole universe was sent.
+   * @returns A Promise resolved when the whole universe was sent.
    * @private
    */
-  private async sendUniverse(): Promise<void> {
+  async _sendUniverse(): Promise<void> {
     return new Promise(resolve => {
       this.port.set({ brk: true, rts: false }, () => {
-        setTimeout(() => {
+        if (this.usleep === null) {
+          setTimeout(() => {
+            this.port.set({ brk: false, rts: false }, () => {
+              setTimeout(() => {
+                this.port.write(this.buffer, () => resolve())
+              }, 1)
+            })
+          }, 1)
+        } else {
+          this.usleep(92)
+
           this.port.set({ brk: false, rts: false }, () => {
-            setTimeout(() => {
-              this.port.write(this.buffer, () => resolve())
-            }, 0)
+            this.usleep!(12)
+            this.port.write(this.buffer, () => resolve())
           })
-        }, 0)
+        }
       })
     })
   }
 
   /**
    * Get the paths of all available devices.
-   * @returns {Promise<string[]>}
    */
   static async listDevices(): Promise<string[]> {
     const allPorts = await SerialPort.list()
@@ -149,8 +164,7 @@ export class EnttecOpenDMXUSBDevice extends EventEmitter<Events> {
 
   /**
    * Get the path of the first available device.
-   * @throws Error When no device is found.
-   * @returns {Promise<string>}
+   * @throws When no device is found.
    */
   static async getFirstAvailableDevice(): Promise<string> {
     const devices = await EnttecOpenDMXUSBDevice.listDevices()
